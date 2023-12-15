ea89452b5182be3737aa6a0fda375377a3751039
Pavel-Const <64945259+Pavel-Const@users.noreply.github.com>
2023-12-15 17:17:10 +0600

    add handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,30 @@
-import './styles/style.scss'
-import './styles/_reset.scss'
-import './styles/forms.scss'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <form class="formBlock">
-    <div class="formBlock__head">
-        <div class="formBlock__title">Вход</div>
-        <div class="formBlock__box">
-            <div class="formBlock__field">
-                <input type="text" class="formBlock__fieldInput" placeholder="">
-                <div class="formBlock__fieldTitle">Логин</div>
-                <div class="formBlock__fieldError">Ошибка</div>
-            </div>
-            <div class="formBlock__field">
-                <input type="password" class="formBlock__fieldInput" placeholder="">
-                <div class="formBlock__fieldTitle">Пароль</div>
-                <div class="formBlock__fieldError">Ошибка</div>
-            </div>
-        </div>
-    </div>
-    <div class="formBlock__actions">
-        <div class="btnBlue formBlock__btn">Авторизоваться</div>
-        <a href="#" class="formBlock__link">Нет аккаунта?</a>
-    </div>
-  </form>
-`
+import Handlebars from 'handlebars';
+import * as Pages from './pages';
 
 
+const pages = {
+    'login': [ Pages.LoginPage, {test: '123'} ],
+};
+
+/*Object.entries(Components).forEach(([ name, component ]) => {
+    Handlebars.registerPartial(name, component);
+});*/
+
+function navigate(page: string) {
+    //@ts-ignore
+    const [ source, context ] = pages[page];
+    const container = document.getElementById('app')!;
+    container.innerHTML = Handlebars.compile(source)(context);
+}
+
+document.addEventListener('DOMContentLoaded', () => navigate('login'));
+
+document.addEventListener('click', e => {
+    //@ts-ignore
+    const page = e.target.getAttribute('page');
+    if (page) {
+        navigate(page);
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }
+});
