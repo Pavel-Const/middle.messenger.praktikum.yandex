9d3e092bb57ff64166212a31ac8c29dffd67dad6
Pavel-Const <64945259+Pavel-Const@users.noreply.github.com>
2024-02-12 15:34:33 +0600

    fix husky

--- mochaSetup.js
+++ mochaSetup.js
@@ -2,16 +2,19 @@ import { JSDOM } from "jsdom";
 import * as Components from "./src/components";
 import { registerComponent } from "./src/core/registerComponent.js";
 
-Object.entries(Components).forEach(
-  ([componentName, component]) => registerComponent(componentName, component)
-);
+Object.entries(Components)
+  .forEach(
+    ([componentName, component]) => registerComponent(componentName, component)
+  );
 
-// jsdom
 const jsdom = new JSDOM("<!doctype html><html><body></body></html>", {
-  url: "http://example.com"
+  url: "http://localhost/"
 });
 
 global.window = jsdom.window;
 global.document = jsdom.window.document;
 global.Node = jsdom.window.Node;
 global.MouseEvent = jsdom.window.MouseEvent;
+global.navigator = {
+  userAgent: "node.js"
+};
