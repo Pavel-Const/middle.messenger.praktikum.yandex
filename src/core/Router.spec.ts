acf8f8e462c91cac3e7959f456dac22782fe8dc9
Pavel-Const <64945259+Pavel-Const@users.noreply.github.com>
2024-02-12 15:31:06 +0600

    fix precommit

--- src/core/Router.spec.ts
+++ src/core/Router.spec.ts
@@ -1,98 +1,61 @@
-// eslint-disable-next-line max-classes-per-file
 import { expect } from "chai";
+import Router from "./Router.ts";
+import Route from "./Route.ts";
 import sinon from "sinon";
-import Router from "./Router";
+import Block from "./Block.ts";
 
 describe("Router", () => {
   let router: Router;
+  let pushStateStub: sinon.SinonStub;
   beforeEach(() => {
-    global.window.history.pushState = sinon.stub();
-    global.window.history.replaceState = sinon.stub();
-    sinon.stub(window.history, "back");
-    sinon.stub(window.history, "forward");
     router = new Router("#app");
-    window.location.pathname = "/initial-path";
+    pushStateStub = sinon.stub(router.history, "pushState");
   });
-
   afterEach(() => {
-    sinon.restore();
+    pushStateStub.restore();
   });
-
-  it("should create an instance of Router", () => {
-    expect(router)
+  
+  it("должен добавлять маршрут с использованием метода use", () => {
+    router.use("/test", Block);
+    const route = router.getRoute("/test");
+    expect(route)
       .to
       .be
-      .an
-      .instanceof(Router);
+      .instanceOf(Route);
   });
-
-  it("use() should register a new route", () => {
-    const path = "/test";
-
-    class BlockFake {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-empty-function
-      constructor() {
-      }
-    }
-
-    router.use(path, BlockFake);
-
-    // Проверяем, что в массиве routes теперь есть один элемент
-    expect((router as any).routes)
-      .to
-      .have
-      .lengthOf(1);
-  });
-
-  it("start()", () => {
-    const spy = sinon.spy((router as any), "_onRoute");
+  it("должен начинать маршрутизацию с использованием метода start", () => {
+    const spy = sinon.spy(router as any, "_onRoute");
     router.start();
-    // Имитация изменения маршрута, если это часть теста
-    window.history.pushState({}, "", "/test");
-    window.dispatchEvent(new PopStateEvent("popstate"));
     // eslint-disable-next-line no-unused-expressions
     expect(spy.called).to.be.true;
   });
-
-  it("go() should change the route", () => {
-    const newPath = "/test-path";
-    const onRouteSpy = sinon.spy(router, "_onRoute");
-    
-    router.go(newPath);
-    
-    // Проверяем, был ли вызван window.history.pushState с правильными аргументами
-    expect(window.history.pushState).to.have.been.calledWith({}, "", newPath);
-    // Проверяем, что _onRoute был вызван после изменения пути
-    expect(onRouteSpy).to.have.been.calledWith(newPath);
+  it("должен перейти на новый маршрут с использованием метода go", () => {
+    router.go("/new-route");
+    // eslint-disable-next-line no-unused-expressions
+    expect(pushStateStub.calledOnce).to.be.true;
+    expect(pushStateStub.firstCall.args)
+      .to
+      .deep
+      .equal([{}, "", "/new-route"]);
   });
-
-  it("back() should navigate one page back in history", () => {
-    const backSpy = sinon.spy(window.history, "back");
+  it("должен вернуться на предыдущий маршрут с использованием метода back", () => {
+    const spy = sinon.spy(router.history, "back");
     router.back();
-
     // eslint-disable-next-line no-unused-expressions
-    expect(backSpy.calledOnce).to.be.true;
-    backSpy.restore();
+    expect(spy.called).to.be.true;
   });
-  it("forward() should navigate one page forward in history", () => {
-    const forwardSpy = sinon.spy(window.history, "forward");
+  it("должен перейти на следующий маршрут с использованием метода forward", () => {
+    const spy = sinon.spy(router.history, "forward");
     router.forward();
-
     // eslint-disable-next-line no-unused-expressions
-    expect(forwardSpy.calledOnce).to.be.true;
-    forwardSpy.restore();
+    expect(spy.called).to.be.true;
   });
-  it("getRoute() should return the correct route for a given path", () => {
-    class TestBlock {}
-    router.use("/test", TestBlock);
-    router.use("/another", TestBlock);
-
-    const testRoute = router.getRoute("/test");
-    const anotherRoute = router.getRoute("/another");
-
-    // eslint-disable-next-line no-unused-expressions
-    expect(testRoute).to.not.be.undefined;
-    // eslint-disable-next-line no-unused-expressions
-    expect(anotherRoute).to.not.be.undefined;
+  it("должен получить маршрут по пути с использованием метода getRoute", () => {
+    router.use("/test", Block);
+    const route = router.getRoute("/test");
+    expect(route)
+      .to
+      .be
+      .instanceOf(Route);
   });
 });
